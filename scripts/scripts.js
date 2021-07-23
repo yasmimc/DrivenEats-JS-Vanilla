43becee5e5ae23e304e9e640fd31013412997016
Yasmim <yasmimcavalcanti.r@gmail.com>
2021-07-23 12:52:55 -0300

    Added js function to change the order buttom from disabled to abled

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,31 +1,34 @@
-function canOrder(meal, drink, dessert) {
+// function canOrder(meal, drink, dessert) {
 
-    if (meal && drink && dessert) {
-        alert("Pode pedir");
-        changeOrderButtom();
-    }
-    else {
-        alert("Selecione todos os itens");
-    }
-}
+//     if (meal && drink && dessert) {
+//         alert("Pode pedir");
+//         changeOrderButtom();
+//     }
+//     else {
+//         alert("Selecione todos os itens");
+//     }
+// }
+
 
 function changeOrderButtom() {
-    alert("eu vou mudar de cor e ficar clicável");
+    let buttomText = document.querySelector(".order-buttom");
+    buttomText.innerHTML = "Fechar Pedido";
+    buttomText.classList.add("abled");
 }
 
-function executeOrder() {
-    alert("vou te redirecionar pro chat do zap");
-}
+// function executeOrder() {
+//     alert("vou te redirecionar pro chat do zap");
+// }
 
-function addNameAndAdress(msg) {
-    const name = prompt("Olá! Qual o seu nome?");
-    const adress = prompt("Agora precisamos do seu endereço:");
+// function addNameAndAdress(msg) {
+//     const name = prompt("Olá! Qual o seu nome?");
+//     const adress = prompt("Agora precisamos do seu endereço:");
 
-    const msg = msg + "Nome: " + name + "Endereço: " + adress;
+//     const msg = msg + "Nome: " + name + "Endereço: " + adress;
 
-    return msg
-}
+//     return msg
+// }
 
-function confirmOrder (order) {
-    alert ("vou confirmar seu pedido, ok?");
-}
+// function confirmOrder (order) {
+//     alert ("vou confirmar seu pedido, ok?");
+// }
