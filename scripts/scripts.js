61da531f74794e5b8bf92c51f3d25380ab188f30
Yasmim <yasmimcavalcanti.r@gmail.com>
2021-07-24 17:40:00 -0300

    Added executOrder function when orderButtom is enabled

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,61 +1,73 @@
-function changeOrderButtom() {
-    let buttomText = document.querySelector(".order-buttom");
-    buttomText.innerHTML = "Fechar Pedido";
-    buttomText.classList.add("abled");
+let whatsappMsg;
+let whatsappNumber = "553184146801";
+function executeOrder() {
+    const isEnabled = document.querySelector(".enabled");
+
+    if (isEnabled) {
+        //exibir tela de confirmação
+        whatsappMsg = encodeURIComponent(whatsappMsg);
+        window.location.href = "https://wa.me/"+ whatsappNumber+"?text="+whatsappMsg;
+    }
+}
+
+function canOrder() {    
+    let sections;
+
+    const mainCourse = document.querySelector(".main-course");
+    const drink = document.querySelector(".drink");
+    const dessert = document.querySelector(".dessert");
+
+    sections = [mainCourse, drink, dessert];
+
+    let selectedOption = Array(3);
+    let allSelected = true;
+    
+    let productName = Array(3);
+    let productPrice = Array(3);
+    let total = 0;
+
+    for (i = 0; i < 3; i++) {        
+        selectedOption[i] = sections[i].querySelector(".selectedOption");
+        if (!selectedOption[i]) {
+            allSelected = false;
+        }
+        else {
+            productName[i] = selectedOption[i].querySelector(".product-name").innerHTML;
+            productPrice[i] = selectedOption[i].querySelector(".product-price").innerHTML;
+
+            // Converting "R$ XX,XX" string prices into XX.XX float values
+            productPrice[i] = parseFloat(productPrice[i].substring(3).replace(',', '.'))
+
+            total = total + productPrice[i];
+        }
+    }
+
+    if (allSelected) {
+        changeOrderButtom();
+        whatsappMsg = "Olá, gostaria de fazer o pedido:" + "\n" +
+            "- Prato: " + productName[0] + "\n" +
+            "- Bebida: " + productName[1] + "\n" +
+            "- Sobremesa: " + productName[2] + "\n" +
+            "Total: " + total;
+    }
 }
 
 function selectOption(option, sectionName) {
-
     const section = document.querySelector("." + sectionName);
-    
     const selectedOption = section.querySelector(".selectedOption");
-    
+
     if (selectedOption) {
         selectedOption.classList.remove("selectedOption");
         option.classList.add("selectedOption");
     }
-    else{
+    else {
         option.classList.add("selectedOption");
-    }    
-
-
+    }
+    canOrder();
 }
 
-
-
-
-
-
-
-
-
-
-
-
-// function canOrder(meal, drink, dessert) {
-
-//     if (meal && drink && dessert) {
-//         alert("Pode pedir");
-//         changeOrderButtom();
-//     }
-//     else {
-//         alert("Selecione todos os itens");
-//     }
-// }
-
-
-
-
-// function executeOrder() {
-//     alert("vou te redirecionar pro chat do zap");
-// }
-
-// function addNameAndAdress(msg) {
-//     const name = prompt("Olá! Qual o seu nome?");
-//     const adress = prompt("Agora precisamos do seu endereço:");
-
-//     const msg = msg + "Nome: " + name + "Endereço: " + adress;
-
-//     return msg
-
-
+function changeOrderButtom() {
+    let buttomText = document.querySelector(".order-buttom");
+    buttomText.innerHTML = "Fechar Pedido";
+    buttomText.classList.add("enabled");
+}
